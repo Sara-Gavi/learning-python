b9831e80b1f41750fe7212b04bc2cab19c3b07ad
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-06-05 11:05:17 +0200

    Wrote comments to understand Python data types (int, float, str, bool) and what each variable does

--- midudev-exercise/01_print.py
+++ midudev-exercise/01_print.py
@@ -1,1 +1,29 @@
+# Esto es un número entero
+my_number = 5
+
+# Esto es un número decimal (float)
+my_float = 3.14
+
+# Esto es una cadena de texto
+my_string = "Hola, mundo"
+
+# Esto es un valor booleano (True o False)
+my_boolean = True
+
+# Puedo imprimir cualquier variable
+print(my_number)
+
+# También puedo sumar números
+result = my_number + my_float
+print(result)
+
+
 print("¡Hola, Mundo!")
+print('Esto tambien funciona con una comilla')
+
+print("python", "es","genial", sep = "-")
+
+print("Esto se imprime", end = "!") 
+print("en una linea")
+
+
